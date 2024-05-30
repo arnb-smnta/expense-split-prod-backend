8315210e2ba9bbb6f4ac259736e2e197f9ecbb21
Arnab Samanta <loveumearnab.2812000@gmail.com>
2024-05-30 19:29:10 +0530

    solved problems 2

--- src/controllers/expense.controller.js
+++ src/controllers/expense.controller.js
@@ -6,6 +6,7 @@ import { ExpenseGroup } from "../models/expensegroup.model.js";
 import { addSplit, clearSplit } from "./expensegroup.controller.js";
 import mongoose from "mongoose";
 import { getLocalPath, getStaticFilePath } from "../helpers/constants.js";
+import { User } from "../models/user.model.js";
 const commonExpenseAggregations = () => {
   return [
     {
